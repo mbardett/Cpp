d5e4e5ccb6bbb4fdee44112286ef07e087b6b954
Mauro Bardetta <mbardett@e4r11p1.42roma.it>
2023-10-30 19:28:31 +0100

    starting cpp08

--- cpp07/ex02/main.cpp
+++ cpp07/ex02/main.cpp
@@ -6,65 +6,65 @@
 /*   By: mbardett <mbardett@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/25 20:50:14 by mbardett          #+#    #+#             */
-/*   Updated: 2023/10/30 15:03:51 by mbardett         ###   ########.fr       */
+/*   Updated: 2023/10/30 17:48:21 by mbardett         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Array.hpp"
 
 //custom Base class for testing
-class Base
-{
-	public:
-			Base(int n);
-			Base();
-			Base(Base & cpy);
-			~Base();
-			int getN() const;
-			void setN(int n);
-			Base&	operator=(Base & rhs);
-			const Base&	operator=(const Base & rhs);
-			Base&	operator+=(Base & rhs);
-			Base&	operator+=(int rhs);
+// class Base
+// {
+// 	public:
+// 			Base(int n);
+// 			Base();
+// 			Base(Base & cpy);
+// 			~Base();
+// 			int getN() const;
+// 			void setN(int n);
+// 			Base&	operator=(Base & rhs);
+// 			const Base&	operator=(const Base & rhs);
+// 			Base&	operator+=(Base & rhs);
+// 			Base&	operator+=(int rhs);
 
-			bool	operator==(Base const & rhs) const;
-			bool	operator!=(Base const & rhs) const;
-			bool	operator>(Base const & rhs) const;
-			bool	operator<(Base const & rhs) const;
-			bool	operator>=(Base const & rhs) const;
-			bool	operator<=(Base const & rhs) const;
+// 			bool	operator==(Base const & rhs) const;
+// 			bool	operator!=(Base const & rhs) const;
+// 			bool	operator>(Base const & rhs) const;
+// 			bool	operator<(Base const & rhs) const;
+// 			bool	operator>=(Base const & rhs) const;
+// 			bool	operator<=(Base const & rhs) const;
 
-	private:
-			int _n;
-};
+// 	private:
+// 			int _n;
+// };
 
-std::ostream&	operator<<(std::ostream& stream, const Base & obj);
+// std::ostream&	operator<<(std::ostream& stream, const Base & obj);
 
 
-	Base::Base  () :_n(std::rand() %10) {}
-	Base::Base  (int n) : _n(n) {}
-	Base::Base  (Base & cpy): _n(cpy.getN()) {*this = cpy;}
-	Base::~Base () {}
+// 	Base::Base  () :_n(std::rand() %10) {}
+// 	Base::Base  (int n) : _n(n) {}
+// 	Base::Base  (Base & cpy): _n(cpy.getN()) {*this = cpy;}
+// 	Base::~Base () {}
 
-	int Base::getN  () const {return this->_n;}
-	void Base::setN  (int n) {this->_n = n;}
+// 	int Base::getN  () const {return this->_n;}
+// 	void Base::setN  (int n) {this->_n = n;}
 
-	Base&	Base::operator=(Base & rhs) {_n = rhs.getN();return *this;}
-	const Base&	Base::operator=(const Base & rhs) {_n = rhs.getN();return *this;}
-	Base&	Base::operator+=(Base & rhs) {_n = _n + rhs.getN();return *this;}
-	Base&	Base::operator+=(int rhs) {_n = _n + rhs;return *this;}
-	bool	Base::operator==(Base const & rhs) const {return (this->_n == rhs.getN());}
-	bool	Base::operator!=(Base const & rhs) const {return (this->_n != rhs.getN());}
-	bool	Base::operator>(Base const & rhs) const {return (this->_n > rhs.getN());}
-	bool	Base::operator<(Base const & rhs) const {return (this->_n < rhs.getN());}
-	bool	Base::operator>=(Base const & rhs) const {return (this->_n >= rhs.getN());}
-	bool	Base::operator<=(Base const & rhs) const {return (this->_n <= rhs.getN());}
+// 	Base&	Base::operator=(Base & rhs) {_n = rhs.getN();return *this;}
+// 	const Base&	Base::operator=(const Base & rhs) {_n = rhs.getN();return *this;}
+// 	Base&	Base::operator+=(Base & rhs) {_n = _n + rhs.getN();return *this;}
+// 	Base&	Base::operator+=(int rhs) {_n = _n + rhs;return *this;}
+// 	bool	Base::operator==(Base const & rhs) const {return (this->_n == rhs.getN());}
+// 	bool	Base::operator!=(Base const & rhs) const {return (this->_n != rhs.getN());}
+// 	bool	Base::operator>(Base const & rhs) const {return (this->_n > rhs.getN());}
+// 	bool	Base::operator<(Base const & rhs) const {return (this->_n < rhs.getN());}
+// 	bool	Base::operator>=(Base const & rhs) const {return (this->_n >= rhs.getN());}
+// 	bool	Base::operator<=(Base const & rhs) const {return (this->_n <= rhs.getN());}
 
-std::ostream&	operator<<(std::ostream& stream, const Base & obj)
-{
-	stream << obj.getN();
-	return stream;
-}
+// std::ostream&	operator<<(std::ostream& stream, const Base & obj)
+// {
+// 	stream << obj.getN();
+// 	return stream;
+// }
 
 int main()
 {
@@ -96,19 +96,19 @@ int main()
 
 
 	
-	std::cout << "\033[38;5;46mTESTING WITH CUSTOM CLASS" << std::endl;
+	// std::cout << "\033[38;5;46mTESTING WITH CUSTOM CLASS" << std::endl;
 
-	Array<Base> a(5);
-	std::cout << "A:" << std::endl;
-	for (size_t i = 0; i < a.size(); i++)
-		a[i] = i;
-	for (size_t i = 0; i < a.size(); i++)
-		std::cout << "at index "<< i << " = " << a[i] << std::endl;
-	std::cout << "\033[0m" << std::endl;
-	std::cout << "\033[38;5;211mTEST for copy of complex type" << std::endl;
-	Array<Base> b;
-	b = a;
-	for (size_t i = 0; i < a.size(); i++)
-		std::cout << "B at index "<< i << " = " << b[i] << std::endl;
-	std::cout << "\033[0m" << std::endl;
+	// Array<Base> a(5);
+	// std::cout << "A:" << std::endl;
+	// for (size_t i = 0; i < a.size(); i++)
+	// 	a[i] = i;
+	// for (size_t i = 0; i < a.size(); i++)
+	// 	std::cout << "at index "<< i << " = " << a[i] << std::endl;
+	// std::cout << "\033[0m" << std::endl;
+	// std::cout << "\033[38;5;211mTEST for copy of complex type" << std::endl;
+	// Array<Base> b;
+	// b = a;
+	// for (size_t i = 0; i < a.size(); i++)
+	// 	std::cout << "B at index "<< i << " = " << b[i] << std::endl;
+	// std::cout << "\033[0m" << std::endl;
 }
